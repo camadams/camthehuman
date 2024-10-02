73a08f6aa04a8cbd460b6f2e27b15fcaf697a9fe
Cameron Adams <camgadams@gmail.com>
2024-10-02 18:57:40 +0200

    fix: vercel deploy

--- src/app/api/testt/route.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "@/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const posts = await db.query.postsTable.findMany();
-  return NextResponse.json(posts);
-}
