9d02ffc3c58acf4e532ba79d6f590033a8fb2388
cameron.adams <camgadams@gmail.com>
2025-03-27 10:26:41 +0200

    fix: fix nav being on top when opened

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,26 +2,24 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const NavLinks = () => (
-  <>
-    <Link className="focus:underline" href="/projects">
-      Projects
-    </Link>
-    {/* <Link className="focus:underline" href="/ideas">Ideas</Link> */}
-    <Link className="focus:underline" href="/blog">
-      Blog
-    </Link>
-    <Link className="focus:underline" href="/about">
-      About
-    </Link>
-  </>
-);
+function NavLinks() {
+  return (
+    <>
+      <Link href="/projects" className="hover:text-blue-500">
+        Projects
+      </Link>
+      <Link href="/blog" className="hover:text-blue-500">
+        Blog
+      </Link>
+      <Link href="/about" className="hover:text-blue-500">
+        About
+      </Link>
+    </>
+  );
+}
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  useEffect(() => {
-    console.log("component rerendered");
-  });
   return (
     <nav className="flex justify-between py-3 px-6 text-lg border-b-2">
       <Link href="/">
@@ -37,7 +35,7 @@ export default function NavBar() {
         ☰
       </div>
       {isOpen && (
-        <div className="fixed inset-0 z-100">
+        <div className="fixed inset-0 z-50">
           <div className="bg-slate-800/70 h-screen flex items-center justify-center" onClick={() => setIsOpen(!isOpen)}>
             <div className="bg-white flex flex-col p-8 gap-4 rounded-md">
               <NavLinks />
